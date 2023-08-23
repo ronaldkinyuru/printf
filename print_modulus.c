c3c46404070025098c8ba5e4da90898615569bd1
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-23 23:31:45 +0300

    check

--- print_modulus.c
+++ print_modulus.c
@@ -5,10 +5,10 @@
  */
 int print_modulus(va_list arg)
 {
-	char val = va_arg(arg, int);
+	char val = va_arg(arg, int); /* check arg type*/
 	int count = 0;
 
-	_putchar(val);
+	_putchar('%%'); /* check*/
 	count ++;
 	return (count);
 }
