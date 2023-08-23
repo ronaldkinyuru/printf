df007a7180cc31d9480b593ba496e6ba5753e4ec
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-23 22:15:40 +0300

    trial

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -12,17 +12,33 @@ int print_binary(va_list arg)
 	int i;
 	int binary[32];/*binary array store*/
 
+	/*calculate number of bits required for the binary representation*/
+	int num_bits = 0;
+	unsigned int temp_val = val;
+	while (temp_val > 0)
+	{
+		temp_val /= 2;
+		num_bits++;
+	}
 	/*Convert the integer to binary representation using an array*/
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < num_bits; i++)
 	{
 		binary[i] = val % 2;
 		val /= 2;
 	}
 
 	/*Print the binary representation*/
-	for (i = 31; i >= 0; i--)
+	for (i = num_bits - 1; i >= 0; i--)
 	{
-		_putchar(binary[i] + '0');
+		if (binary[i] == 1)
+		{
+			_putchar(binary[i] + '0');
+			count++;
+		}
+	}
+	if (count == 0)
+	{
+		_putchar('0');
 		count++;
 	}
 
