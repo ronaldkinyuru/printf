a24b87e73f8026cc4f8dd0a3371b46900c69e86c
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-08-25 19:13:56 +0300

    u decimal

--- print_unsigned_decimal.c
+++ print_unsigned_decimal.c
@@ -9,9 +9,33 @@ int print_unsigned_decimal(va_list arg)
 	int count 0, i, num_digits = 1;
 	unsigned long int val = va_arg(arg, unsigned long int);
 
-	if (val < 0)
-	{
+	unsigned long int temp = val;
+
+	while (temp != 0)
 	{
 		temp /= 10;
 		digits++;
 	}
+	if (val == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	else
+	{
+		char unsigned_string[11]; /*The maximum value of an unsigned 32-bit integer is 4,294,967,295. This number has 10 digits*/
+		i = 0;
+		while (num !=0)
+		{
+			unsigned_string[i] = (val % 10) + 48;
+			val /= 10;
+			i++;
+		}
+		for (i = num_digits - 1; i >= 0; i--)
+		{
+			_putchar(unsigned_string[i]);
+			count++;
+		}
+	}
+	return (count);
+}
---
+++ print_unsigned_octal.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+  * print_octal - deal with o
+  * @arg: ocatl to be printed
+  * Return: count
+  */
+int print_ocatl(va_list, arg)
+{
+	int 
